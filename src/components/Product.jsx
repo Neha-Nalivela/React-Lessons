a08d1c5f5ac041cd30ccd6b86a3e1c6a1211d661
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-23 20:11:00 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,13 @@
-export default function Product() {
-  return <h3>This is Product component</h3>;
+export default function Product(props) {
+  const name = props.name;
+  const price = props.price + 15000; 
+  const desc = "This is Product Description."
+  return(
+    <div>
+      <h3>This is Product component - {name}---{price}</h3>
+      <p>
+        {desc}
+      </p>
+    </div>
+  );
 }
