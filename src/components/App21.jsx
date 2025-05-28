8b0c20390b28feac3712adfdb55c1ee272a8b14b
Neha-Nalivela <nehareddynalivela@gmail.com>
2025-05-28 19:24:30 +0530

    update

--- src/components/App21.jsx
+++ src/components/App21.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Register from "./Register";
 import Login from "./Login";
+import "./App21.css";
 import { useState } from "react";
 import { createContext } from "react";
 export const AppContext = createContext();
@@ -11,11 +12,11 @@ export default function App21() {
       <AppContext.Provider value={{ users, setUsers }}>
         <h1>App21</h1>
         <h2>Assignment</h2>
-        <div style={{ display: "flex" }}>
-          <div>
+        <div style={{ display: "flex", justifyContent: "center", alignContent: "center"}}>
+          <div className="Register">
             <Register />
           </div>
-          <div>
+          <div className="Login">
             <Login />
           </div>
         </div>
